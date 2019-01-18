57f54f4db9f2d32f20d88d9748d614c1fe1fcbe6
Antonio Mendoza <antonio.mendoza.mmg@gmail.com>
2019-01-17 22:06:31 -0600

    Lectura de base de datos

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
+from .models import Empleado, Archivo
 
 # Create your views here.
 def home (request):
+    empleado = Empleado.objects.all().order_by('id')
     context = {
         'title' : 'SS | Carga de Expedientes',
-        'target' : 'CARGAR EXPEDIENTE'
+        'target' : 'CARGAR EXPEDIENTE',
+        'empleados' : empleado
     }
     return render(request, 'home/home.html', context)
 
