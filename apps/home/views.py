659757883887883408e211d1f43984368c0c31ec
Carlos-Salina <rusaca.821@gmail.com>
2019-01-20 14:41:40 -0600

    login

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
 from django.core.files.storage import FileSystemStorage
 from apps.home.models import Empleado
@@ -6,6 +7,8 @@ from apps.home.models import Empleado
 #fs.save(f.name, f)
 
 # Create your views here.
+
+@login_required
 def home (request):
     empleado = Empleado.objects.all().order_by('id')
     context = {
--- ss/settings.py
+++ ss/settings.py
@@ -136,3 +136,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = '/home'
+LOGIN_URL ='/'
